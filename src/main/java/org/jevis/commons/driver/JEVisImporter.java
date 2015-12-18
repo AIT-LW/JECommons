bae281a5ac9fddd721b3d4a5ada45bb4864ed1b3
AIT-LW <werner.lamprecht.fl@ait.ac.at>
2015-12-18 13:28:01 +0100

    Remove attribute string
    JEVisImporter:76
    Every value gets as Node attribute the string "Imported by JEDataCollector" => overhead for big data

--- src/main/java/org/jevis/commons/driver/JEVisImporter.java
+++ src/main/java/org/jevis/commons/driver/JEVisImporter.java
@@ -74,7 +74,7 @@ public class JEVisImporter implements Importer {
                     }
                     List<JEVisSample> samples = onlineToSampleMap.get(onlineData);
                     DateTime convertedDate = TimeConverter.convertTime(_timezone, s.getDate());
-                    JEVisSample sample = onlineData.getAttribute("Value").buildSample(convertedDate, s.getValue(), "Imported by JEDataCollector");
+                    JEVisSample sample = onlineData.getAttribute("Value").buildSample(convertedDate, s.getValue(), "");
                     samples.add(sample);
                 } catch (Exception ex1) {
                     Logger.getLogger(JEVisImporter.class.getName()).log(Level.ERROR, "Error while importing samples", ex1);
